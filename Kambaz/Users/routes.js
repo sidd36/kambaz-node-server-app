919ca176687612249b307879172d8969f0b8f0f2
siddharth_sanjeev <sanjeev.si@northeastern.edu>
2025-04-16 21:09:57 -0400

    Hw 6 commit 3

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -109,24 +109,4 @@ export default function UserRoutes(app) {
     };
     
     app.post("/api/users/current/courses", createCourse);
-
-    const findCoursesForUser = async (req, res) => {
-        const currentUser = req.session["currentUser"];
-        if (!currentUser) {
-          res.sendStatus(401);
-          return;
-        }
-        if (currentUser.role === "ADMIN") {
-          const courses = await courseDao.findAllCourses();
-          res.json(courses);
-          return;
-        }
-        let { uid } = req.params;
-        if (uid === "current") {
-          uid = currentUser._id;
-        }
-        const courses = await enrollmentsDao.findCoursesForUser(uid);
-        res.json(courses);
-      };
-      app.get("/api/users/:uid/courses", findCoursesForUser);
 }
